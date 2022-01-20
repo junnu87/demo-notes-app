b5fa1826be999801622f61c8b47ac3e2f796396c
Gerald Lee <junnu_87@hotmail.com>
2022-01-20 11:15:26 +0200

    Adding the API

---
+++ stacks/StorageStack.js
@@ -0,0 +1,19 @@
+import * as sst from '@serverless-stack/resources';
+
+export default class StorageStack extends sst.Stack {
+  // Puclic reference to the table
+  table;
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    // Create the DynamoDB table
+    this.table = new sst.Table(this, 'Notes', {
+      fields: {
+        userId: sst.TableFieldType.STRING,
+        noteId: sst.TableFieldType.STRING,
+      },
+      primaryIndex: { partitionKey: 'userId', sortKey: 'noteId' },
+    });
+  }
+}
