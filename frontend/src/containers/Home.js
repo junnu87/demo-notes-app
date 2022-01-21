98a7d062fd070446eef42756828194fbc26561f9
Gerald Lee <junnu_87@hotmail.com>
2022-01-21 12:04:50 +0200

    Setting up our React app

---
+++ frontend/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='Home'>
+      <div className='lander'>
+        <h1>Sales White Spot</h1>
+        <p className='text-muted'>Simple show sales app</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
